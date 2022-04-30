a234182c6aa94ec0fad5bbe531cc17de08da5c11
Dmitriy <uplmtq@gmail.com>
2022-04-30 13:28:00 +0300

    Все функции теперь находятся в классах

--- utils/secondary_utils.py
+++ utils/secondary_utils.py
@@ -1,9 +1,14 @@
 import requests
 
 
-def check_internet_by_url(url):
-    try:
-        requests.get(url)
-        return True
-    except requests.ConnectionError:
-        return False
+class SecondaryUtils:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def check_internet_by_url(url):
+        try:
+            requests.get(url)
+            return True
+        except requests.ConnectionError:
+            return False
