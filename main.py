d75c255df170e4f07b2381ba2e24d9f3323de3e5
Dmitriy Sobovyy <uplmtq@gmail.com>
2022-05-04 15:11:41 +0300

    Правильное импортирование модулей

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import sys
+
 from PyQt5 import QtWidgets
 
-from config.config import ConfigParser
+from config.config_parser import ConfigParser
 from database.database import DataBase
+from exceptions import Exceptions
 from gui.gui import Authorization
+from utils.parser_utils import ParserUtils
+from utils.security_utils import SecurityUtils
 
 
 def main():
-    import sys
     config = ConfigParser("config/config.ini")
-    database = DataBase('sqlite3.sqlite3')
+    parser_utils = ParserUtils(config)
+    security_utils = SecurityUtils(config)
+    exceptions = Exceptions(config)
+
+    database = DataBase("database.sqlite3", config, parser_utils, security_utils, exceptions)
 
     database.delete_database()
     database.create_all_tables()
