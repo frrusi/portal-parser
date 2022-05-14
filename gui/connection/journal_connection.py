f173bbca760e839b560099e4fdd426a4ef8e1324
Dmitriy <uplmtq@gmail.com>
2022-05-14 17:45:47 +0300

    Исправление проблем с видимостью некоторых модулей, замена "хардкод"-констант на константы из конфигурационного файла

--- gui/connection/journal_connection.py
+++ gui/connection/journal_connection.py
@@ -1,16 +1,17 @@
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QTableWidgetItem
 
 from gui.windows import journal_window
 
 
 class JournalWindow(QtWidgets.QMainWindow, journal_window.Ui_JournalWindow):
-    def __init__(self, database):
+    def __init__(self, database, config):
         super(JournalWindow, self).__init__()
         self.setupUi(self)
 
         self.database = database
-        self.item = None
+        self.config = config
 
     def initialization(self, group, semester, subject):
         get_all_date = self.database.get_data(subject, semester)
@@ -31,17 +32,17 @@ class JournalWindow(QtWidgets.QMainWindow, journal_window.Ui_JournalWindow):
             'QTableWidget QTableCornerButton::section {background-color: #ffffff;}')
         self.table.setStyleSheet('selection-background-color: #ffffe0; selection-color: #000000')
 
-        self.table.setHorizontalHeaderLabels(['Список группы'] + get_all_date)
+        self.table.setHorizontalHeaderLabels([self.config.group_list_message] + get_all_date)
 
         for index, value in enumerate(get_all_student):
-            self.item = QTableWidgetItem(value)
-            self.item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-            self.table.setItem(index, 0, self.item)
+            item = QTableWidgetItem(value)
+            item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+            self.table.setItem(index, 0, item)
             self.table.resizeColumnsToContents()
 
         for index, value in enumerate(get_all_marks):
-            self.item = QTableWidgetItem(value)
-            self.item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-            self.item.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
-            self.table.setItem(index // len(get_all_date), index % len(get_all_date) + 1, self.item)
+            item = QTableWidgetItem(value)
+            item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+            item.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+            self.table.setItem(index // len(get_all_date), index % len(get_all_date) + 1, item)
             self.table.resizeColumnsToContents()
