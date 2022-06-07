6c749b27f37c8c504729f09a1af78a40d9c43cba
Dmitriy Sobovyy <uplmtq@gmail.com>
2022-06-07 11:31:56 +0300

    Создание объекта класса, отвечающего за второстепенные методы для графического интерфейса

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from database.database import DataBase
 from exceptions import Exceptions
 from gui.connection.auth_connection import AuthWindow
 from utils import ParserUtils, SecurityUtils, SecondaryUtils
+from utils.gui_utils import GuiUtils
 
 
 def main():
@@ -14,13 +15,14 @@ def main():
     parser_utils = ParserUtils(config)
     security_utils = SecurityUtils(config)
     secondary_utils = SecondaryUtils()
+    gui_utils = GuiUtils()
     exceptions = Exceptions(config)
 
     database = DataBase("database.sqlite3", config, parser_utils, security_utils, exceptions)
     database.create_all_tables()
 
     app = QtWidgets.QApplication(sys.argv)
-    window = AuthWindow(config, database, exceptions, parser_utils, security_utils, secondary_utils)
+    window = AuthWindow(config, database, exceptions, parser_utils, security_utils, secondary_utils, gui_utils)
     window.show()
     sys.exit(app.exec_())
 
