9a8a8266ded2001ca1fb8de5e04fba52a65fd54e
Dmitriy Sobovyy <uplmtq@gmail.com>
2022-06-07 11:32:16 +0300

    Метод проверки соответствия пароля

--- utils/security_utils.py
+++ utils/security_utils.py
@@ -1,4 +1,5 @@
 import hashlib
+import re
 import uuid
 
 
@@ -20,3 +21,20 @@ class SecurityUtils:
         if self.check_password(hashed_password, entered_password):
             return self.config.successful_code
         return self.config.error_code
+
+    @staticmethod
+    def check_password_steps(password):
+        length_error = len(password) >= 8
+        uppercase_error = re.search(r"[A-Z]", password) is not None
+        lowercase_error = re.search(r"[a-z]", password) is not None
+        digit_error = re.search(r"\d", password) is not None
+        symbol_error = re.search(r"[!@#$%^&*]", password) is not None
+
+        return {
+            'length_error': length_error,
+            'uppercase_error': uppercase_error,
+            'lowercase_error': lowercase_error,
+            'digit_error': digit_error,
+            'symbol_error': symbol_error,
+        }
+
